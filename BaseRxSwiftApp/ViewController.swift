6d3ff6c3ed86957cdd6d7ac2a60c430617c50d5c
nguyen toan <nktoan.dev@gmail.com>
2025-08-06 20:50:51 +0700

    Initial Commit

---
+++ BaseRxSwiftApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BaseRxSwiftApp
+//
+//  Created by Bé Gạo on 6/8/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
